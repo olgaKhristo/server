408b14cc1dcf435babf9463dfd2c360dc246d3a5
olgaKhristo <olga84h@gmail.com>
2023-09-12 10:21:29 +0100

    end of rirst

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const http = require('http'); // inbilt to node
-const server = http.createServer((req, res) => {
-    res.statusCode = 404; // status code to display mistake
-  res.end(); // creates the server and sends the response
-});
-server.listen(3000, () => {
-  console.log('Server ready');
-});
+
+
+
+
+
+
+
+
+// const http = require('http'); // inbilt to node
+// const server = http.createServer((req, res) => {
+//     console.log(req.url);
+//     console.log(req.method);
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end("<img src='https://pbs.twimg.com/media/Dj8XlmjV4AEzsvr.jpg'>")
+// ; // creates the server and sends the response
+// });
+// server.listen(3000, () => {
+//   console.log('Server ready');
+// });
 
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 node index.js - to console log in bash terninal
 added nodemoon - npm i nodemode -D
 npm run dev (to see how its run the changes in Bas) - press ctrl C - to exit this mood
+
+Express.js, which is a popular and widely-used web application framework for Node.js. Express.js simplifies the process of building robust and scalable web applications and APIs by providing a set of features and tools to handle common tasks, such as routing, middleware, and handling HTTP requests and responses.
